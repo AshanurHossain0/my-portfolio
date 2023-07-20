72c07cfc655881fbda5f77a416cae0225f0c104a
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-07-20 12:58:42 +0530

    about_updated2

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -4,7 +4,7 @@ import aboutImg from '../images/about.png'
 const About = () => {
   return (
     <div className='flex flex-col md:flex-row justify-evenly m:0 md:mt-2 w-full h-auto'>
-      <div className='flex justify-center w-full md:w-1/3  md:mb-0 mb-8 h-auto'>
+      <div className='flex justify-center w-full md:w-1/3  md:mb-0 mb-8 h-[440px] md:h-[500px]'>
         <img className='rounded-md w-11/12 ' src={aboutImg} alt='about'  />
       </div>
       <div className='w-full md:w-3/5 '>
@@ -12,10 +12,10 @@ const About = () => {
         <div className='flex flex-col mt-2 md:mt-6 p-2  h-auto'>
           <p className='font-bold text-2xl md:text-4xl mb-1 md:mb-2'>Hi, I'm Ashanur Hossain.</p>
           <p className='text-lg md:text-xl'>I am from West Bengal, India.</p>
-          <p className='text-lg md:text-xl'>As a passionate MERN stack developer with over 6 months of experience, I find immense joy in the world of web development and the ever-evolving technologies that come with it. Building dynamic and efficient applications is not just a profession but a creative outlet that allows me to shape ideas into reality, while constantly challenging and expanding my skills.</p>
-          <p className='text-lg md:text-xl'>Apart from my love for coding, I have another fulfilling role as a mathematics teacher for class nine and ten students. Guiding young minds and helping them on their educational journey.</p>
-          <p className='text-lg md:text-xl'>In my leisure time, I spend time with my family.</p>
-          <p className='text-lg md:text-xl'>Programming is not merely a job; it's a genuine passion of mine. The thrill of solving complex problems and the satisfaction of creating elegant solutions keep me motivated and inspired.</p>
+          <p className='text-lg md:text-xl mb-2'>As a passionate MERN stack developer with over 6 months of experience, I find immense joy in the world of web development and the ever-evolving technologies that come with it. Building dynamic and efficient applications is not just a profession but a creative outlet that allows me to shape ideas into reality, while constantly challenging and expanding my skills.</p>
+          <p className='text-lg md:text-xl mb-2'>Apart from my love for coding, I have another fulfilling role as a mathematics teacher for class nine and ten students. Guiding young minds and helping them on their educational journey.</p>
+          <p className='text-lg md:text-xl mb-2'>In my leisure time, I spend time with my family.</p>
+          <p className='text-lg md:text-xl mb-4'>Programming is not merely a job; it's a genuine passion of mine. The thrill of solving complex problems and the satisfaction of creating elegant solutions keep me motivated and inspired.</p>
         </div>
       </div>
 
