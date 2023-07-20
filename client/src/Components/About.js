b8623d0a439c0faa0632b35e37b02c067cf6eb55
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-07-20 12:50:42 +0530

    about_updated

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -4,12 +4,12 @@ import aboutImg from '../images/about.png'
 const About = () => {
   return (
     <div className='flex flex-col md:flex-row justify-evenly m:0 md:mt-2 w-full h-auto'>
-      <div className='flex justify-center w-full md:w-1/3  md:mb-0 mb-2 h-auto'>
+      <div className='flex justify-center w-full md:w-1/3  md:mb-0 mb-8 h-auto'>
         <img className='rounded-md w-11/12 ' src={aboutImg} alt='about'  />
       </div>
-      <div className='w-full md:w-3/5  '>
+      <div className='w-full md:w-3/5 '>
         <p className='text-center font-semibold text-xl md:text-3xl text-blue-600 mx-2'>Who am I?</p>
-        <div className='flex flex-col md:mt-6 p-2  h-auto'>
+        <div className='flex flex-col mt-2 md:mt-6 p-2  h-auto'>
           <p className='font-bold text-2xl md:text-4xl mb-1 md:mb-2'>Hi, I'm Ashanur Hossain.</p>
           <p className='text-lg md:text-xl'>I am from West Bengal, India.</p>
           <p className='text-lg md:text-xl'>As a passionate MERN stack developer with over 6 months of experience, I find immense joy in the world of web development and the ever-evolving technologies that come with it. Building dynamic and efficient applications is not just a profession but a creative outlet that allows me to shape ideas into reality, while constantly challenging and expanding my skills.</p>
